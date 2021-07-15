48d0c8c8b88f33b87088eb4bf0d914ad8ce9a8f7
Seokan <67941435+seokan-jeong@users.noreply.github.com>
2021-07-16 00:13:12 +0900

    Add files via upload

---
+++ Navigation/Navigation/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by 정석환 on 2021/07/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController, EditDelegate {
+    
+    let imgOn = UIImage(named: "lamp_on.png")
+    let imgOff = UIImage(named: "lamp_off.png")
+    
+    var isOn = true
+    
+    @IBOutlet var txtMessage: UITextField!
+    @IBOutlet var imgView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        imgView.image = imgOn
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let editViewController = segue.destination as! EditViewController
+        if segue.identifier == "editButton" {
+            // 버튼을 클릭한 경우
+            editViewController.textWayValue = "segue : use button"
+        }else if segue.identifier == "editBarButton" {
+            // 바 버튼을 클릭한 경우
+            editViewController.textWayValue = "segue : use bar button"
+        }
+        editViewController.textMessage = txtMessage.text!
+        editViewController.isOn = isOn
+        editViewController.delegate = self
+    }
+
+    func didMessageEditDone(_ controller: EditViewController, message: String) {
+        txtMessage.text = message
+    }
+    
+    func didImageOnOffDone(_ controller: EditViewController, isOn: Bool) {
+        if isOn {
+            imgView.image = imgOn
+            self.isOn = true
+        } else {
+            imgView.image = imgOff
+            self.isOn = false
+        }
+    }
+}
+
